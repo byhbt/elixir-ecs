172cfbcf0b45b734710d8cc3435fc80479e1060e
Thanh Huynh <huynhbathanh@gmail.com>
2021-12-04 20:12:05 +0700

    Temporarily disable ecto

--- lib/demo_elixir_app/application.ex
+++ lib/demo_elixir_app/application.ex
@@ -8,7 +8,7 @@ defmodule DemoElixirApp.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      DemoElixirApp.Repo,
+      # DemoElixirApp.Repo,
       # Start the Telemetry supervisor
       DemoElixirAppWeb.Telemetry,
       # Start the PubSub system
